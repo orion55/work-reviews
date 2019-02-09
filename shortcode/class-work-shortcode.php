503f381436499e9fdf6cc41a5427d8568a41be93
Orion55 <grol55@mail.ru>
2019-02-09 22:52:20 +0500

    вёрстка

--- shortcode/class-work-shortcode.php
+++ shortcode/class-work-shortcode.php
@@ -17,8 +17,35 @@ class Work_Shortcode
         self::$add_script = true;
 
         $html = "
-        <div id='work-reviews'>
-        <h1>work-reviews</h1>
+        <div id='work-reviews' class='work'>
+            <div class='work__box'>
+                <div class='work__container'>
+                    <div class='work__title'>Отзывы о <span class='work__textcolor'>нашей работе</span></div>
+                    <div class='work__holder'>
+                        <div class='work__btn-slider'>
+                            <div class='work__slider-btn work__slider--prev'>
+                               <svg aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"angle-left\" class=\"svg-inline--fa fa-angle-left fa-w-8\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 256 512\"><path fill=\"currentColor\" d=\"M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z\"></path></svg> 
+                            </div>
+                            <div class='work__slider-btn work__slider--next'>
+                                <svg aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"angle-right\" class=\"svg-inline--fa fa-angle-right fa-w-8\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 256 512\"><path fill=\"currentColor\" d=\"M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z\"></path></svg>
+                            </div>
+                        </div>
+                        <div class='work__slider'>
+                            <div class='work__item'>
+                                <div class='work__text'>
+                                    <svg aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"quote-left\" class=\"svg-inline--fa fa-quote-left fa-w-16\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path fill=\"currentColor\" d=\"M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z\"></path></svg>
+                                    <span class='work__paragraph'>Клиниговая компания действительно чудо. Я человек пунктуальный , дорожу каждой минуткой и не люблю опаздывать сама. Ребята приехали во время, даже чуть раньше, свою работу сделали изумительно. Теперь наслаждаюсь своей чистой уютной квартиркой.</span>                                    
+                                </div>
+                                <div class='work__name'>Елена</div>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div class='work__btn'>
+                        <button type='button' class='btn btn--result hvr-pop'>Оставьте свой отзыв</button>
+                    </div>                    
+                </div>
+                <div class='work__container'></div>
+            </div>
         </div>
         ";
 

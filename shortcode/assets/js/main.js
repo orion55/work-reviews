26cc40110f1ad7fd7232a56b4070f6762e28fa36
orion55 <grol55@mail.ru>
2019-02-12 14:44:38 +0500

    contact form

--- shortcode/assets/js/main.js
+++ shortcode/assets/js/main.js
@@ -3,7 +3,12 @@
 jQuery(document).ready(function ($) {
   var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
-    spaceBetween: 20
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    }
   });
 
   $('#work__slider--next').click(function () {
